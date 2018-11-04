0fc422acac6284b899c1939538544f0e39286480
magor <magorsy3@gmail.com>
2018-11-04 13:35:50 +0000

    4/11/2018

--- src/app/Components/auth/auth.component.ts
+++ src/app/Components/auth/auth.component.ts
@@ -9,25 +9,26 @@ import { AuthService } from '../../Services/auth.service';
 })
 export class AuthComponent implements OnInit {
 
-  public login;
-  public password;
-  public nom;
-  public prenom;
-  public profession;
-  public pays;
-  public day;
-  public moth;
-  public year;
-  public email;
-  public pwd1;
-  public pwd2;
-  public phone;
+  public login:string;
+  public password:string;
+  public nom:string;
+  public prenom:string;
+  public profession:string;
+  public pays:string;
+  public day:string;
+  public moth:string;
+  public year:string;
+  public email:string;
+  public pwd1:string;
+  public pwd2:string;
+  public phone:string;
 
   public section = 1;
+  public errorLogin=false;
 
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService, private router: Router) {}
+//  constructor(private router: Router) {}
 
   ngOnInit() {
     this.login = '';
@@ -35,19 +36,30 @@ export class AuthComponent implements OnInit {
   }
 
   longin() {
-    // this.authService.logint(JSON.stringify({'login' : this.login, 'pwd' : this.password })).subscribe(data => {
-    //     console.log(data);
-    //     this.router.navigate(['/accueil']);
-    // });
-    this.router.navigate(['/accueil']);
+    this.authService.logint(JSON.stringify({'login' : this.login, 'pwd' : this.password })).then(data => {
+         console.log(data);
+         switch(parseInt(data.etat)){
+           case 1:{
+               this.router.navigate(['/accueil']);
+               break;
+           }
+           case 0:{
+             console.log("donnee incorrect");
+             this.errorLogin=true;
+             break;
+           }
+           default:{break;}
+         }
+     });
+    //this.router.navigate(['/accueil']);
   }
 
   sinup() {
-    // this.authService.sinUp(JSON.stringify({'login' : this.login, 'pwd' : this.password })).subscribe(data => {
-    //     console.log(data);
-    //     this.router.navigate(['/accueil']);
-    // });
-    this.router.navigate(['/accueil']);
+     this.authService.sinUp(JSON.stringify({'prenom':this.prenom,'nom':this.nom,'email':this.email,'phone':this.phone,'pwd':this.pwd1,'country':this.pays,'profession':this.profession })).then(data => {
+         console.log(data);
+         //this.router.navigate(['/accueil']);
+     });
+    //this.router.navigate(['/accueil']);
   }
 
 
--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -2,55 +2,65 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+//import { Http,Headers} from '@angular/http';
 
 
-@Injectable({
+/*@Injectable({
   providedIn: 'root'
-})
+})*/
+@Injectable()
 export class AuthService {
 
-  private link = 'http://localhost/crmTNTBackend/index.php';
+  private link = 'http://127.0.0.1:8000';
 
-  private httpOptions = {
+ private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/x-www-form-urlencoded'
     })
   };
 
-  constructor(private http: HttpClient) { }
+ constructor(private http:HttpClient) {
+  // this.headers.append('Content-Type','application/x-www-form-urlencoded');
+  }
 
 
-  logint(infos: String): Observable <any> {
+  logint(infos: String):Promise<any>{
+    let datas = 'params=' + infos;
+    let Url = this.link + '/connexion/';
+    return new Promise((resolve,reject)=> {
+      this.http.post<any>(Url, datas,this.httpOptions).subscribe(rep=>{
+            console.log(rep);
+            resolve(rep);
+      });
+     });
+  }
+
+
+ sinUp(infos: String):Promise<any>{
+    let datas = 'params=' + infos;
+    console.log(datas)
+    let Url = this.link + '/connexion/newUser';
+    return new Promise((resolve,reject)=>{
+      this.http.post<any>(Url, datas, this.httpOptions).subscribe(rep =>{
+        console.log(rep);
+        resolve(rep);
+      });
+    });
+
+  }
+
+
+/*  public sinUp(infos: String){
     const datas = 'params=' + infos;
     const Url = this.link + '';
     return this.http.post< any >(Url, datas, this.httpOptions)
     .pipe(
        catchError(this.handleError)
     );
-  }
+  }*/
 
 
-  public logout(infos: String): Observable <any> {
-    const datas = 'params=' + infos;
-    const Url = this.link + '';
-    return this.http.post< any >(Url, datas, this.httpOptions)
-    .pipe(
-       catchError(this.handleError)
-    );
-  }
-
-
-  public sinUp(infos: String): Observable <any> {
-    const datas = 'params=' + infos;
-    const Url = this.link + '';
-    return this.http.post< any >(Url, datas, this.httpOptions)
-    .pipe(
-       catchError(this.handleError)
-    );
-  }
-
-
-  private handleError(error: HttpErrorResponse) {
+  /*private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
@@ -63,6 +73,6 @@ export class AuthService {
     }
     // return an observable with a user-facing error message
     return throwError('Something bad happened; please try again later.');
-  }
+  }*/
 
 }
